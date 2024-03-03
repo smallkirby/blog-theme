83aee9a4f0c9ca642127946d0acbcf389a2348c2
smallkirby <ssmallkirby@gmail.com>
2024-03-03 12:09:29 +0900

    init go template HTML formatter
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,13 @@ module.exports = {
   trailingComma: 'es5',
   singleQuote: true,
   bracketSpacing: true,
+  overrides: [
+    {
+      files: ['*.html'],
+      options: {
+        parser: 'go-template',
+      },
+    },
+  ],
+  plugins: ['prettier-plugin-go-template'],
 };
